f3f67902b8ec6b761135e406e93aef467b06f12c
kkatsuko <nicolepouco@gmail.com>
2026-05-21 01:14:13 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("App iniciado ✨");
+const todayTask = document.getElementById("today-task");
+
+const buttons = document.querySelectorAll(".type-btn");
+
+buttons.forEach((button) => {
+
+  button.addEventListener("click", () => {
+
+    const text = button.innerText;
+
+    todayTask.innerText = text;
+
+  });
+
+});
