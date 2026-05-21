92a12b2b218ef48c2cf4b3371a1ff69918bb2d01
kkatsuko <nicolepouco@gmail.com>
2026-05-21 01:25:39 -0300

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,12 @@ buttons.forEach((button) => {
   });
 
 });
+
+if ("serviceWorker" in navigator) {
+
+  navigator.serviceWorker.register("service-worker.js")
+    .then(() => {
+      console.log("Service Worker registrado");
+    });
+
+}
